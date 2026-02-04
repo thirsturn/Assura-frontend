384d4e3bfeafc387f18be72bf5119f538bb40a71
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-02-04 15:49:17 +0530

    feature/created the login page

---
+++ frontend/src/app/features/auth/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "appLogin",
+    templateUrl: "./login.html",
+    styleUrls: ["./login.css"]
+})
+
+export class LoginComponent {
+    
+}
