780474c87d76d4e89b7ff5ccb762ef33366cbd97
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-02-06 17:10:54 +0530

    feat: connected with backend

--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }  from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private apiUrl = 'http://localhost:5171/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  // register
+  register(userData: any) {
+    return this.http.post(`${this.apiUrl}/setup`, userData);
+  }
+
+  // login
+  login(credentials: any) {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
 }
--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Auth } from "../../../core/services/auth";
 
 @Component({
     selector: "appLogin",
@@ -8,6 +9,25 @@ import { Component } from "@angular/core";
 
 export class LoginComponent {
     isLoggingIn: boolean = false;
+    loginData = {
+        username: '',
+        password: '',
+    };
+
+    constructor(private auth: Auth) {}
+
+    onSubmit(){
+        console.log('Logging in...', this.loginData);
+
+        this.auth.login(this.loginData).subscribe({
+            next: (response) => {
+                console.log('Login successful!', response);// TODO: store token
+            },
+             error: (err) => {
+                console.log('Login Failed', err); // TODO: show error message
+             }
+        })
+    }
 
     // wrap the existing login logic
     async handleLogin(){
